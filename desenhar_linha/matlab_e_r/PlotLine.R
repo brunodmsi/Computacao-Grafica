9444d5b6f9fc0bd6922310b5dcc55378b4e44f2c
Bruno De Masi <brunodemasi1@gmail.com>
2020-02-20 10:52:13 -0300

    feat: add desenhar linha em R e Matlab

---
+++ desenhar_linha/matlab_e_r/PlotLine.R
@@ -0,0 +1,8 @@
+x1 <- readline(prompt="Entre com o X1 =>")
+x1 <- readline(prompt="Entre com o X2 =>")
+y1 <- readline(prompt="Entre com o Y1 =>")
+y2 <- readline(prompt="Entre com o Y2 =>")
+
+xzao = c(x1,x2)
+yzao = c(y1,y2)
+plot(xzao,yzao,type="l",lty=1)
---
+++ desenhar_linha/matlab_e_r/README.md
@@ -0,0 +1,2 @@
+# Desenhar linhas em R e Matlab
+Neste a idéia é a mesma, mas a execução em uma linguagem diferente. Basta abrir os arquivos em seus respectivos compiladores e rodar.
