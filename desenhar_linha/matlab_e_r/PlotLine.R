59df0562e3f13c19f306fc1ce6ad496c2861f970
Bruno De Masi <brunodemasi1@gmail.com>
2020-02-20 19:21:37 -0500

    fix: fixed R code

--- desenhar_linha/matlab_e_r/PlotLine.R
+++ desenhar_linha/matlab_e_r/PlotLine.R
@@ -1,8 +1,13 @@
-x1 <- readline(prompt="Entre com o X1 =>")
-x1 <- readline(prompt="Entre com o X2 =>")
-y1 <- readline(prompt="Entre com o Y1 =>")
-y2 <- readline(prompt="Entre com o Y2 =>")
+PlotLine <- function()
+{ 
+  x1 <- readline(prompt="Entre com o X1 => ")
+  x2 <- readline(prompt="Entre com o X2 => ")
+  y1 <- readline(prompt="Entre com o Y1 => ")
+  y2 <- readline(prompt="Entre com o Y2 => ")
+  
+  Eixo_X = c(x1,x2)
+  Eixo_Y = c(y1,y2)
+  plot(Eixo_X,Eixo_Y,type="l",lty=1)
+}
 
-xzao = c(x1,x2)
-yzao = c(y1,y2)
-plot(xzao,yzao,type="l",lty=1)
+print(PlotLine())
